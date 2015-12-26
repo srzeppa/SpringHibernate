eaac0612362d9127d04783d9ac1f3ff4752f6cd1
Sławek Rzeppa <srzeppa@sigma.ug.edu.pl>
2015-12-26 14:03:21 +0100

    ClientDao + ClientDaoImpl

---
+++ src/main/java/com/srzeppa/dao/ClientDao.java
@@ -0,0 +1,14 @@
+package com.srzeppa.dao;
+
+import com.srzeppa.domain.Client;
+import java.util.List;
+
+public interface ClientDao {
+	
+	void addClient(Client client);
+	List<Client> getAllClients();
+	void deleteClient(Client client);
+	Client getClientById(int id);
+	void deleteClientById(int id);
+
+}
---
+++ src/main/java/com/srzeppa/dao/ClientDaoImpl.java
@@ -0,0 +1,34 @@
+package com.srzeppa.dao;
+
+import java.util.List;
+
+import com.srzeppa.domain.Client;
+
+public class ClientDaoImpl implements ClientDao{
+
+	public void addClient(Client client) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<Client> getAllClients() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void deleteClient(Client client) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Client getClientById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void deleteClientById(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
